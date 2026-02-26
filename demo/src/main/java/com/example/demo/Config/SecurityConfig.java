f1801e8db54660faefce74f2d9f79dd7e4e994d9
yasser baragh <ssrcrb@gmail.com>
2026-02-26 15:48:01 +0100

    spring security

--- demo/src/main/java/com/example/demo/Config/SecurityConfig.java
+++ demo/src/main/java/com/example/demo/Config/SecurityConfig.java
@@ -39,6 +39,9 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.POST,"/api/users/login").permitAll()
                         .requestMatchers(HttpMethod.POST,"/api/users/register").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/products/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/categories/**").authenticated()
+                        .requestMatchers(HttpMethod.GET, "/api/tags/**").authenticated()
                         .anyRequest().authenticated()
                 )
                 .authenticationManager(authenticationManager);
